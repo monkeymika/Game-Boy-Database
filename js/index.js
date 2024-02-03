b6094fbfd954453f496a67fcd71b081247df70eb
Mika <michael.aubras@gmail.com>
2024-02-03 16:28:38 +0100

    home almost done

--- js/index.js
+++ js/index.js
@@ -91,7 +91,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 /* Initialize Swiper */
-var swiper = new Swiper(".slide-container", {
+
+var swiper = new Swiper(".last-game-added-section .slide-container", {
 	slidesPerView: 4,
 	spaceBetween: 20,
 	sliderPerGroup: 4,
@@ -100,13 +101,85 @@ var swiper = new Swiper(".slide-container", {
 	fade: "true",
 	grabCursor: "true",
 	pagination: {
-		el: ".swiper-pagination",
+		el: ".swiper-pagination1",
 		clickable: true,
 		dynamicBullets: true,
 	},
 	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
+		nextEl: ".swiper-button-next1",
+		prevEl: ".swiper-button-prev1",
+	},
+
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		520: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		1000: {
+			slidesPerView: 4,
+		},
+	},
+});
+
+
+// Initialize Swiper for Recommended Games
+var swiperMostSeen = new Swiper(".most-seen-on-database .slide-container", {
+	// ... your configuration ...
+	slidesPerView: 4,
+	spaceBetween: 20,
+	sliderPerGroup: 4,
+	loop: true,
+	centerSlide: "true",
+	fade: "true",
+	grabCursor: "true",
+	pagination: {
+		el: ".swiper-pagination2",
+		clickable: true,
+		dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next2",
+		prevEl: ".swiper-button-prev2",
+	},
+
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		520: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+		1000: {
+			slidesPerView: 4,
+		},
+	},
+});
+// Initialize Swiper for Some Videos
+var swiperMostSeen = new Swiper(".videos .slide-container", {
+	// ... your configuration ...
+	slidesPerView: 4,
+	spaceBetween: 20,
+	sliderPerGroup: 4,
+	loop: true,
+	centerSlide: "true",
+	fade: "true",
+	grabCursor: "true",
+	pagination: {
+		el: ".swiper-pagination3",
+		clickable: true,
+		dynamicBullets: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next3",
+		prevEl: ".swiper-button-prev3",
 	},
 
 	breakpoints: {
